1719cee8a0612a9730826571dc72fc7ad106bfbd
rovanov <reamehd@gmail.com>
2025-09-24 11:48:16 +0300

    Update views.py

--- Lab2/mywebpage/flatpages/views.py
+++ Lab2/mywebpage/flatpages/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django import template
+# Импорт стандартных компонентов Django
+from django.shortcuts import render  # Функция для рендеринга шаблонов
+from django import template  # Модуль для работы с шаблонами
+from django.http import HttpResponse  # Класс для создания HTTP-ответов
 
-from django.http import HttpResponse
-
+# Определение представления (view) для главной страницы
 def home(request):
+    # Рендеринг шаблона static_handler.html и возврат ответа
     return render(request, 'templates/static_handler.html')
